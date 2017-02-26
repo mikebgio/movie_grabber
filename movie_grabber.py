8dbde6917cd7c582c422751c9adef2d5f9307b1b
Mike Giordano <mgiord1987@gmail.com>
2017-02-25 19:25:57 -0500

    Added function for grabbing MovieGuide comment from submissions

--- movie_grabber.py
+++ movie_grabber.py
@@ -1,5 +1,5 @@
 # CLI Tool for downloading Full Movies on YouTube via Reddit.
-# V0.01 App which gets links from r/fullmoviesonyoutube
+# V0.02 App which gets links from r/fullmoviesonyoutube
 
 import praw
 #import config
@@ -14,10 +14,12 @@ def get_submissions(subreddit):
     """Grabs the 10 newest submissions"""
     submissions = []   # This will be the list of 10 links, each stored as dict
     for submission in subreddit.new(limit=10): # Iterate through 10 newest submissions
+        guide = get_guide_comment(submission)
         subdata = dict(title = submission.title, # Store submission data as dictionary of title:{necessary data}
             subid = submission.id,  
             link = submission.url,
-            score = submission.score)
+            score = submission.score,
+            guide = guide)
         submissions.append(subdata) # Store each submission dictionary in list
     return submissions
 
@@ -26,6 +28,15 @@ def print_submissions(subslist):
     out with index and score pretty-like"""
     for i, v in enumerate(subslist):
         print(str(i) + ": " + v['title'] + " :: SCORE: " + str(v['score']) + div)
+
+def get_guide_comment(submission):
+    """Checks the submission fed to it for a comment from MovieGuide.
+    This comment bot provides a brief IMDB Blurb."""
+    author = "MovieGuide"
+    for comments in submission.comments.list():
+        if comments.author == author:
+            return comments.body
+
 
 subslist = get_submissions(subreddit)
 print_submissions(subslist)
