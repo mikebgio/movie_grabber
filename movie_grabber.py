e478a9bce03bc9a275653949b9d95d90def51dc2
Mike Giordano <mgiord1987@gmail.com>
2017-03-16 14:56:57 -0400

    Did some PEP8 cleanup

--- movie_grabber.py
+++ movie_grabber.py
@@ -3,42 +3,46 @@
 # V0.03 App which gets links from r/fullmoviesonyoutube
 # and now grabs MovieGuide comments for IMDb info
 
-import praw     #for interacting w/Reddit
-import spinner
-#import config  #this will contain download options
-#import time    #This will be used for request-limiting (maybe)
-import os      #This will be used for storing history
+import praw     # for interacting w/Reddit
+import spinner  # bring in the loading animation
+# import config  # this will contain download options
+# import time    # This will be used for request-limiting (maybe)
+# import os       # This will be used for storing history
 
-spinney = spinner.Spinner() #create a spinner from spinner.py
-div = "\n" + ("~*" * 20) # This is just a divider for printing
-guide_error = "Sorry, there is no info available for this movie." # Error msg for no guide comment
+spinney = spinner.Spinner()  # create a spinner from spinner.py
+div = "\n" + ("~*" * 20)  # This is just a divider for printing
+guide_error = "Sorry, there is no info available for this movie."  # Error msg for no guide comment
 
-reddit = praw.Reddit('movie_grabber', user_agent = 'movie_grabber user agent')
+reddit = praw.Reddit('movie_grabber', user_agent='movie_grabber user agent')
 subreddit = reddit.subreddit('fullmoviesonyoutube')
+
 
 def get_submissions(subreddit):
     """Grabs the 10 newest submissions"""
     print("Grabbing 10 movies...")
-    spinney.start() #animate spinner while running rest of method
+    spinney.start()  # animate spinner while running rest of method
     submissions = []   # This will be the list of 10 links, each stored as dict
-    for submission in subreddit.new(limit=10): # Iterate through 10 newest submissions
-        guide = get_guide_comment(submission)
-        if guide == None:
+    for submission in subreddit.new(limit=10):  # Iterate through 10 newest submissions
+        guide = get_guide_comment(submission)  # find MovieGuide comment
+        if guide is None:  # if MovieGuide comment cannot be found, load error message
             guide = guide_error
-        subdata = dict(title = submission.title, # Store submission data as dictionary of title:{necessary data}
-            subid = submission.id,  
-            link = submission.url,
-            score = submission.score,
-            guide = guide)
-        submissions.append(subdata) # Store each submission dictionary in list
-    spinney.stop() #stop animation
+        subdata = dict(  # Store submission data as dictionary of title:{necessary data}
+            title=submission.title,
+            subid=submission.id,
+            link=submission.url,
+            score=submission.score,
+            guide=guide)  # store MovieGuide comment for user retrieval
+        submissions.append(subdata)  # Store each submission dictionary in list
+    spinney.stop()  # stop animation
     return submissions
+
 
 def print_submissions(subslist):
     """Extracts titles from the submissions list and prints them
     out with index and score pretty-like"""
     for i, v in enumerate(subslist):
         print(str(i) + ": " + v['title'] + " :: SCORE: " + str(v['score']) + div)
+
 
 def get_guide_comment(submission):
     """Checks the submission fed to it for a comment from MovieGuide.
@@ -48,14 +52,15 @@ def get_guide_comment(submission):
         if comments.author == author:
             return comments.body
 
+
 def print_more_info(query, subslist):
     """Provide user Title and Guide text if they enter <info-request-command>"""
-    guide = subslist[query]['guide'] #load full guide comment source
+    guide = subslist[query]['guide']  # load full guide comment source
     if guide == guide_error:
         description = guide
         cast_crew = ""
     else:
-        cast_crew_start = guide.find("min]") + 6 #load start of the cast list
+        cast_crew_start = guide.find("min]") + 6  # load start of the cast list
         cast_crew_end = cast_crew_start + guide[cast_crew_start:].find("\n\n") + 4
         cast_crew = guide[cast_crew_start:cast_crew_end]
         description_start = guide.find('>')
@@ -63,7 +68,11 @@ def print_more_info(query, subslist):
         description = guide[description_start:description_end]
     print(str(query) + ": " + subslist[query]['title'])
     print(cast_crew)
-    print(description) #Just prints RAW Source for now (will fix with string stuff)
+    print(description)  # Just prints RAW Source for now (will fix with string stuff)
+
+
+def log_viewed():
+    pass
 
 
 subslist = get_submissions(subreddit)
--- spinner.py
+++ spinner.py
@@ -6,18 +6,21 @@ import sys
 import time
 import threading
 
+
 class Spinner:
     busy = False
     delay = 0.1
 
     @staticmethod
     def spinning_cursor():
-        while 1: 
-            for cursor in '|/-\\': yield cursor
+        while 1:
+            for cursor in '|/-\\':
+                yield cursor
 
     def __init__(self, delay=None):
         self.spinner_generator = self.spinning_cursor()
-        if delay and float(delay): self.delay = delay
+        if delay and float(delay):
+            self.delay = delay
 
     def spinner_task(self):
         while self.busy:
